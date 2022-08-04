8998044b932e16c53638c60188346b8e13c27ef8
dsychoi <87092042+dsychoi@users.noreply.github.com>
2022-08-03 21:21:19 -0600

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
